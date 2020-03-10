83f510d853ffd874b4d354f6d4de6123972ce8e4
Benjamin Dinh <bhdinh@ucdavis.edu>
2020-03-10 11:16:25 -0700

    done

---
+++ Intersection.cpp
@@ -0,0 +1,9 @@
+//
+// Created by Benjamin Dinh on 3/9/20.
+//
+
+#include "Intersection.h"
+
+Intersection::Intersection() {
+  isKnown = false;
+}
---
+++ Intersection.h
@@ -0,0 +1,14 @@
+//
+// Created by Benjamin Dinh on 3/9/20.
+//
+
+#ifndef P5_INTERSECTION_H
+#define P5_INTERSECTION_H
+
+class Intersection{
+  bool isKnown;
+
+  Intersection():
+};
+
+#endif //P5_INTERSECTION_H
