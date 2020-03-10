3ea3ebe65a3ad340e5f0a16966232409a1a44fcf
Benjamin Dinh <bhdinh@ucdavis.edu>
2020-03-09 18:22:08 -0700

    Initial commit

---
+++ FindMax.cpp
@@ -0,0 +1,12 @@
+// Author Sean Davis
+
+#include "FindMax.h"
+
+FindMax::FindMax(int citySize, const StreetBlock *streets, int numStreets)
+{
+} // FindMax()
+
+void FindMax::find(Coordinate fireHouses[4], Coordinate *furthest, int *time)
+{
+} // find()
+
---
+++ FindMax.h
@@ -0,0 +1,12 @@
+#ifndef _FINDMAX_H
+#define	_FINDMAX_H
+#include "RunFind.h"
+
+class FindMax {
+public:
+  FindMax(int citySize, const StreetBlock *streets, int numStreets);
+  void find(Coordinate fireHouses[4], Coordinate *furthest, int *time);
+}; // class FindMax
+
+#endif	/* _FINDMAX_H */
+
